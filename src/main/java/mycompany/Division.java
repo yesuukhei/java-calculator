0514fffd1a4a6a52234a630f93e285484fe7b584
Yesuukehi <yesuu1028@gmail.com>
2025-05-14 16:27:34 +0800

    asdf

--- src/main/java/mycompany/Division.java
+++ src/main/java/mycompany/Division.java
@@ -3,7 +3,7 @@ package mycompany;
 public class Division {
  public double divide(double a, double b) {
     if (b == 0) {
-        throw new IllegalArgumentException("Тэгээр хуваах боломжгүй");
+        throw new IllegalArgumentException("Тэгээр хувах боломжгүй");
     }
     return a / b;
  }
