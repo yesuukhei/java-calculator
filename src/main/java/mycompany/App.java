76ddea3f258d3b72bad9dc43a44484c8c7b44028
Yesuukehi <yesuu1028@gmail.com>
2025-05-14 14:26:08 +0800

    worklow test

--- src/main/java/mycompany/App.java
+++ src/main/java/mycompany/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "workflow ajilj ehle" );
+        System.out.println( "workflow test" );
     }
 }
