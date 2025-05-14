5c16d5cd564d814dc7134a5ddd92d8e217c3a8d6
Yesuukehi <yesuu1028@gmail.com>
2025-05-14 13:42:55 +0800

    first commit

---
+++ src/main/java/mycompany/App.java
@@ -0,0 +1,13 @@
+package mycompany;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
