2ffd6f04c0e22cac7bb2e7d8addceef5ce2138b8
Yesuukehi <yesuu1028@gmail.com>
2025-05-14 17:09:25 +0800

    123

--- src/main/java/mycompany/Division.java
+++ src/main/java/mycompany/Division.java
@@ -3,7 +3,7 @@ package mycompany;
 public class Division {
  public double divide(double a, double b) {
     if (b == 0) {
-        throw new IllegalArgumentException("Тэгээр хувах боломжгүй");
+        throw new IllegalArgumentException("Ened aldaa zaslaa.");
     }
     return a / b;
  }
